5a3814176610cd82810d4295b20336f78f0d0365
Mika88 <mikaspivak@gmail.com>
2018-12-20 20:40:25 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -11,6 +11,7 @@ class CouponsController < ApplicationController
   end
 
   def create
+    binding.pry
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
